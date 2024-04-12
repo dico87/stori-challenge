c4e859c477623bd549199ce2c45cc8ff2abff1c1
Diego Cortés <dico87@gmail.com>
2024-04-12 16:53:34 -0500

    remove logs

--- internal/transactions/transactions.go
+++ internal/transactions/transactions.go
@@ -1,9 +1,7 @@
 package transactions
 
 import (
-	"fmt"
 	"github.com/dico87/stori-challenge/internal/transactions/domain"
-	"time"
 )
 
 type Sender interface {
@@ -33,23 +31,15 @@ func NewTransactions(sender Sender, reader Reader, repository TransactionReposit
 }
 
 func (t Transactions) Process() error {
-	startTime := time.Now()
 	transactions, err := t.reader.Read("large_sample.csv")
-	endTime := time.Now()
-	fmt.Printf("Time read : [%d]", endTime.Sub(startTime).Milliseconds())
+
 	if err != nil {
 		return err
 	}
 
-	startTime = time.Now()
 	summary := domain.NewSummary(transactions.TotalBalance(), transactions.GroupByMonth(), transactions.Average())
-	endTime = time.Now()
-	fmt.Printf("Time build summary : [%d]", endTime.Sub(startTime).Milliseconds())
 
-	startTime = time.Now()
 	err = t.repository.Create(summary, transactions)
-	endTime = time.Now()
-	fmt.Printf("Time save in database : [%d]", endTime.Sub(startTime).Milliseconds())
 
 	if err != nil {
 		return err
